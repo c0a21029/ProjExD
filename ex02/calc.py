48a9708981088f627c9783369ef67ef89fe8853d
kagiwada <c0a2102913@edu.teu.ac.jp>
2022-11-29 14:38:14 +0900

    ＋、＝

--- ex02/calc.py
+++ ex02/calc.py
@@ -18,7 +18,13 @@ for i in range(9,-1,-1):
     if k%3==0:
         c+=1
         k=0
-        
+#記号
+ope=["+","="]
+for i in ope:
+        button=tk.Button(root,text=i,width=4,height=2,font=("",30))
+        button.grid(row=c,column=k)
+        k+=1
+
 #練習入力欄
 entry=tk.Entry(root,justify="right",width=10,font=("",40))
 entry.grid(row=0,column=0,columnspan=3)
