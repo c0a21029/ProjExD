55f70f08939c5790b3acffac14b6cdad52f43734
kagiwada <c0a2102913@edu.teu.ac.jp>
2022-11-29 14:34:55 +0900

    入力欄

--- ex02/calc.py
+++ ex02/calc.py
@@ -8,7 +8,8 @@ def Button_click(event):
     txt=event.widget["text"]
     tkm.showinfo("押すな",str(txt)+"のボタンがクリックされました")
 
-c=0;k=0
+#練習ボタン
+c=1;k=0
 for i in range(9,-1,-1):
     button=tk.Button(root,text=i,width=4,height=2,font=("",30))
     button.bind("<1>",Button_click)
@@ -18,5 +19,8 @@ for i in range(9,-1,-1):
         c+=1
         k=0
         
+#練習入力欄
+entry=tk.Entry(root,justify="right",width=10,font=("",40))
+entry.grid(row=0,column=0,columnspan=3)
 
 root.mainloop()
