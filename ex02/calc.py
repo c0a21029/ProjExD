bda064293b050ec87110000d3b1dd7d6d1a32618
kagiwada <c0a2102913@edu.teu.ac.jp>
2022-11-29 15:50:37 +0900

    四則演算の追加

--- ex02/calc.py
+++ ex02/calc.py
@@ -2,8 +2,9 @@ import tkinter as tk
 import tkinter.messagebox as tkm
 
 root=tk.Tk()
-root.geometry("300x500")
+root.geometry("400x500")
 
+#クリック時の挙動
 def Button_click(event):
     txt=event.widget["text"]
     if txt=="=":
@@ -21,7 +22,7 @@ def Change_cl(event):
 def Re_cl(event):
     event.widget["bg"]="white"
 
-#練習ボタン
+#ボタン
 c=1;k=0
 for i in range(8,-1,-1):
     button=tk.Button(root,text=3*(3-c)+k+1,width=4,height=2,font=("",30))
@@ -32,12 +33,11 @@ for i in range(8,-1,-1):
     k+=1
     if k%3==0:
         c+=1
-        k=0
+        k=0 
 
-button=tk.Button(root,text="a",width=4,height=2,font=("",30))
-button.grid(row=1,column=4)
 #記号
-ope=["+",0,"="]
+ope=["%",0,"."]
+ope2=["*","-","+","="]
 for i in ope:
         button=tk.Button(root,text=i,width=4,height=2,font=("",30))
         button.bind("<1>",Button_click)
@@ -45,9 +45,23 @@ for i in ope:
         button.bind("<Leave>",Re_cl)
         button.grid(row=c,column=k)
         k+=1
+c=1
+for i in ope2:
+        button=tk.Button(root,text=i,width=4,height=2,font=("",30))
+        button.bind("<1>",Button_click)
+        button.bind("<Enter>",Change_cl)
+        button.bind("<Leave>",Re_cl)
+        button.grid(row=c,column=4)
+        c+=1
 
-#練習入力欄
+#追加機能
+button=tk.Button(root,text="tes",width=4,font=("",30))
+button.bind("<1>",Button_click)
+button.bind("<Enter>",Change_cl)
+button.bind("<Leave>",Re_cl)
+button.grid(row=0,column=4)
+#入力欄
 entry=tk.Entry(root,justify="right",width=10,font=("",40))
-entry.grid(row=0,column=0,columnspan=3)
+entry.grid(row=0,column=0,columnspan=4)
 
 root.mainloop()
