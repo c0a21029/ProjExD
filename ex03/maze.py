65d7f3de31f21a7051c10214d6c47a5f921a324a
kagiwada <c0a2102913@edu.teu.ac.jp>
2022-12-06 14:31:24 +0900

    とり

--- ex03/maze.py
+++ ex03/maze.py
@@ -2,5 +2,15 @@ import tkinter as tk
 
 
 if __name__=="__main__":
+    #ウィンドウ生成
     root=tk.Tk()
-    
+    root.title("迷えるこうかとん")
+    canv=tk.Canvas(root,width=1500,height=900,bg="black")
+    #画像読み込み～表示
+    tori=tk.PhotoImage(file="fig/8.png")
+    cx,cy=300,400
+    canv.create_image(cx,cy,image=tori,tag="hato")
+    canv.pack()
+
+    key=""
+    root.mainloop()
